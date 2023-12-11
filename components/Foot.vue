742a81c73fc1687e3f5263a2f08f877f13e3f4b9
a4akki <akhilsharma870@gmail.com>
2023-12-10 20:23:31 -0800

    .

--- components/Foot.vue
+++ components/Foot.vue
@@ -1,12 +1,12 @@
 <template>
-  <section class="py-8 lg:py-20 bg-black">
-    <div class="container mx-auto px-4">
+  <section class="py-8 lg:py-20 bg-black ">
+    <div class="container mx-auto px-4 item-center">
       <div class="flex flex-wrap justify-center lg:justify-between items-center mx-20">
         <div class="w-full lg:w-2/6 px-4 mb-6 lg:mb-0 text-center lg:text-left">
           <p class="text-white text-sm lg:text-base">Made with ❤️ in India.</p>
         </div>
-        <div class="w-full lg:w-4/6 px-4 text-center lg:text-right">
-          <div class="flex flex-wrap items-center justify-center lg:justify-end mx-0 lg:mx-20">
+        <div class="w-full lg:w-4/6 text-center lg:text-right ">
+          <div class="flex flex-wrap items-center justify-center lg:justify-end mx-0 ">
             <a
               class="inline-block lg:mr-4 mb-4 lg:mb-0 py-2 px-8 text-sm lg:text-base text-white font-medium leading-normal bg-orange-400 hover:bg-orange-600 rounded"
               href="https://t.me/besodaku"
